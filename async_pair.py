925d98f45c0eec30b6c5c42db7dd92ce586b24c7
J. Nick Koston <nick@koston.org>
2021-01-16 13:42:58 -1000

    adj

--- async_pair.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import logging
-
-from pylutron_caseta.pairing import async_pair
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(async_pair("192.168.106.62"))
