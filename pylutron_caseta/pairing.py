12b73f597ff660406d50e5b4b8d78eb5cf4560e5
J. Nick Koston <nick@koston.org>
2021-01-16 13:39:44 -1000

    wip

--- pylutron_caseta/pairing.py
+++ pylutron_caseta/pairing.py
@@ -29,29 +29,6 @@ PAIR_CA = "ca"
 PAIR_VERSION = "version"
 
 
-def _generate_private_key():
-    LOGGER.info("Generating a new private key...")
-    return rsa.generate_private_key(
-        public_exponent=65537, key_size=2048, backend=default_backend()
-    )
-
-
-def _convert_private_key_to_pem(private_key):
-    return private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption(),
-    )
-
-
-def _generate_csr(private_key):
-    return (
-        x509.CertificateSigningRequestBuilder()
-        .subject_name(CERT_SUBJECT)
-        .sign(private_key, hashes.SHA256(), default_backend())
-    )
-
-
 class JsonSocket:
     """A socket that reads and writes json objects."""
 
@@ -76,10 +53,36 @@ class JsonSocket:
         LOGGER.debug("sent: %s", buffer)
 
 
-async def _async_generate_certificate(server_addr, ssl_context, csr):
-    import pprint
+async def async_pair(server_addr):
+    """Pair with a lutron bridge."""
+    loop = asyncio.get_running_loop()
+    csr, key_bytes_pem, ssl_context = await loop.run_in_executor(
+        None, _generate_csr_with_ssl_context, server_addr
+    )
 
-    pprint.pprint(["_async_generate_certificate", server_addr, ssl_context, csr])
+    cert_pem, ca_pem = await _async_generate_certificate(server_addr, ssl_context, csr)
+
+    signed_ssl_context = await loop.run_in_executor(
+        None, _generate_signed_ssl_context, key_bytes_pem, cert_pem, ca_pem
+    )
+
+    leap_response = await _async_verify_certificate(server_addr, signed_ssl_context)
+
+    version = leap_response["Body"]["PingResponse"]["LEAPVersion"]
+
+    LOGGER.debug(
+        "Successfully connected to bridge, running LEAP Server version %s", version
+    )
+
+    return {
+        PAIR_KEY: key_bytes_pem.decode("ASCII"),
+        PAIR_CERT: cert_pem,
+        PAIR_CA: ca_pem,
+        PAIR_VERSION: version,
+    }
+
+
+async def _async_generate_certificate(server_addr, ssl_context, csr):
     reader, writer = await asyncio.wait_for(
         asyncio.open_connection(
             server_addr,
@@ -91,9 +94,6 @@ async def _async_generate_certificate(server_addr, ssl_context, csr):
         timeout=SOCKET_TIMEOUT,
     )
     json_socket = JsonSocket(reader, writer)
-
-    pprint.pprint("json_socket")
-
     LOGGER.info("Press the small black button on the back of the Caseta bridge...")
     while True:
         message = await json_socket.async_read_json(BUTTON_PRESS_TIMEOUT)
@@ -133,7 +133,30 @@ async def _async_generate_certificate(server_addr, ssl_context, csr):
     return signing_result["Certificate"], signing_result["RootCertificate"]
 
 
-async def async_verify_certificate(server_addr, signed_ssl_context):
+def _generate_private_key():
+    LOGGER.info("Generating a new private key...")
+    return rsa.generate_private_key(
+        public_exponent=65537, key_size=2048, backend=default_backend()
+    )
+
+
+def _convert_private_key_to_pem(private_key):
+    return private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption(),
+    )
+
+
+def _generate_csr(private_key):
+    return (
+        x509.CertificateSigningRequestBuilder()
+        .subject_name(CERT_SUBJECT)
+        .sign(private_key, hashes.SHA256(), default_backend())
+    )
+
+
+async def _async_verify_certificate(server_addr, signed_ssl_context):
     reader, writer = await asyncio.wait_for(
         asyncio.open_connection(
             server_addr,
@@ -157,38 +180,6 @@ async def async_verify_certificate(server_addr, signed_ssl_context):
         leap_response = await json_socket.async_read_json(SOCKET_TIMEOUT)
         if leap_response.get("CommuniqueType") == "ReadResponse":
             return leap_response
-
-
-async def async_pair(server_addr):
-    """Pair with a lutron bridge."""
-    loop = asyncio.get_running_loop()
-    csr, key_bytes_pem, ssl_context = await loop.run_in_executor(
-        None, _generate_csr_with_ssl_context, server_addr
-    )
-
-    import pprint
-
-    pprint.pprint([csr, key_bytes_pem, ssl_context])
-    cert_pem, ca_pem = await _async_generate_certificate(server_addr, ssl_context, csr)
-
-    signed_ssl_context = await loop.run_in_executor(
-        None, _generate_signed_ssl_context, key_bytes_pem, cert_pem, ca_pem
-    )
-
-    leap_response = await async_verify_certificate(server_addr, signed_ssl_context)
-
-    version = leap_response["Body"]["PingResponse"]["LEAPVersion"]
-
-    LOGGER.debug(
-        "Successfully connected to bridge, running LEAP Server version %s", version
-    )
-
-    return {
-        PAIR_KEY: key_bytes_pem.decode("ASCII"),
-        PAIR_CERT: cert_pem,
-        PAIR_CA: ca_pem,
-        PAIR_VERSION: version,
-    }
 
 
 def _generate_csr_with_ssl_context(server_addr):
